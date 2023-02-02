170aa964a83fd62da0d76c6d2733488541c35343
Thor the Fixer <120442441+Jaymasters440@users.noreply.github.com>
2023-02-02 09:05:51 -0800

    Add date and time and local memory and time shift

--- script.js
+++ script.js
@@ -2,13 +2,31 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
+ var currentHour= dayjs().hour();
+  $( ".description" ).each(function() {
+   var hour=parseInt(this.dataset.hour)
+   if(currentHour===hour){
+    $( this ).addClass( "present" );
+   }else if(currentHour > hour){
+    $( this ).addClass( "past" );
+   }else if(currentHour < hour){
+    $( this ).addClass( "future" );
+   }
+    
+  });
+  
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
-  //
+  $(".btn").each(function(){
+   $(this).click(function(){
+   
+    localStorage.setItem($(this).parent().attr('id'),$(this).prev().val());
+   })
+  })
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
